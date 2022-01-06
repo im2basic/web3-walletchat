f3f9a19742254b2c2c25da4038d37e276315ff29
tata <basically.tata@gmail.com>
2022-01-05 23:57:03 -0600

    W3WC- Added login and connection to crypto wallet with moralis

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
+}
+
+export default MyApp;
